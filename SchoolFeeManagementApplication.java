6bba0d5dc4e467e4c65d7cf36223a521a89ca48f
1yawar <131624525+1yawar@users.noreply.github.com>
2023-10-22 01:09:07 +0530

    Add files via upload

---
+++ SchoolFeeManagementApplication.java
@@ -0,0 +1,17 @@
+package in.co.Edviron.SchoolFeeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class SchoolFeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolFeeManagementApplication.class, args);
+
+	}
+
+}
